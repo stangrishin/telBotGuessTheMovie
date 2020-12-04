73513aedbc788dbcb4c16896ee7cdb58f5e4beef
Stanislav Grishin <stanislav.v.grishin@gmail.com>
2020-12-04 15:55:12 +0300

    Do not change while it works

--- app.js
+++ app.js
@@ -1,97 +1,107 @@
-require('dotenv').config()
-const fs = require('fs')
-const { Telegraf } = require('telegraf')
+require('dotenv').config();
+const { db, massivFilmov } = require('./db');
+const fs = require('fs');
+const { Telegraf } = require('telegraf');
 const fetch = require('node-fetch');
 
-const URL = `https://dictionaryapi.com/api/v3/references/thesaurus/json/big?key=a0c8a291-99f2-4382-9b4a-91c91a3e7ac5`
-const phrase = 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son'
-const wordsOnly = phrase.split(' ')
+const phrase =
+  db[massivFilmov[Math.floor(Math.random() * massivFilmov.length)]];
+console.log(phrase);
+const wordsOnly = phrase.split(' ');
 
-async function getData(phraise){
-  const response = await fetch(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${phraise}?key=a0c8a291-99f2-4382-9b4a-91c91a3e7ac5`)
-  const rez = await response.json()
-  if(rez[0].meta.ants.length){
-    return rez[0].meta.ants
-  }else{
-  return rez[0].meta.syns
+async function getListOfAntsAndSyns(phraise) {
+  const response = await fetch(
+    `https://dictionaryapi.com/api/v3/references/thesaurus/json/${phraise}?key=a0c8a291-99f2-4382-9b4a-91c91a3e7ac5`
+  );
+  const rez = await response.json();
+  if (rez[0]) {
+    if (rez[0]?.meta?.ants?.length) {
+      return rez[0].meta.ants;
+    } else if (rez[0]?.meta?.syns?.length) {
+      return rez[0].meta.syns;
+    }
   }
 }
 
-async function myFunc(giveMelist){
-  let newList = []
+async function getTheLineOfNewEpigraf(giveMelist) {
+  let newList = [];
   for (let i = 0; i < giveMelist.length; i++) {
-    if (giveMelist[i].length>4){
-    let antonym = await getData(giveMelist[i])
-    console.log(antonym[0])
-    let randomIndex = Math.floor(Math.random()*(antonym[0].length))
-    newList.push(antonym[0][randomIndex])    
-    } else{
-      newList.push(giveMelist[i])
+    if (giveMelist[i].length > 4) {
+      let antonym = await getListOfAntsAndSyns(giveMelist[i]);
+
+      if (Array.isArray(antonym)) {
+        let randomIndex = Math.floor(Math.random() * antonym[0].length);
+        newList.push(antonym[0][randomIndex]);
+      } else {
+        newList.push(giveMelist[i]);
+      }
+    } else {
+      newList.push(giveMelist[i]);
     }
-  };
-  // console.log(newList.join(' '))
+  }
+  return newList.join(' ');
 }
 
-// myFunc(wordsOnly)
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Greetings, '));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('text', async (ctx) => {
+  await allInOneDone();
+  ctx.replyWithAudio({ source: './speech.ogg' });
+});
+bot.launch();
 
-// const bot = new Telegraf(process.env.BOT_TOKEN)
-// bot.start((ctx) => ctx.reply('Greetings, '))
-// bot.help((ctx) => ctx.reply('Send me a sticker'))
-// bot.on('text', (ctx) => {
+const key = process.env.KEY;
 
-// ctx.reply('Hello, ' + ctx.message.from.first_name)
-// })
-// bot.launch()
-const key = 't1.9euelZrIy83Lx5GVlI6YnZqemZ6Ti-3rnpWajMyclp2TnY6XnpnJipaXkIvl8_cOW1wB-u8HThwo_N3z904JWgH67wdOHCj8.rbYDwcfdZE23zBx0805l0vCBGyP8Ki62rtur7o73ghDIGYFmEVFUeQ0b865_ytBUX96y8MMrkL11NKy3DDX1BQ'
-
-async function howto(){
-  const response = await fetch("https://translate.api.cloud.yandex.net/translate/v2/translate",{
-    method: 'POST',
-    headers:{
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${key}`
-    },
-    body:JSON.stringify({
-    folder_id: "b1gnfe4dlmtcvgp604dj",
-    texts: ["How", "Old"],
-    targetLanguageCode: "ru"
-    })
-  })
-  const rez = await response.json()
-  console.log(rez)
+async function letsTranslateThePhraseIntoRussian(whatToTranslate) {
+  const response = await fetch(
+    'https://translate.api.cloud.yandex.net/translate/v2/translate',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${key}`,
+      },
+      body: JSON.stringify({
+        folder_id: 'b1gnfe4dlmtcvgp604dj',
+        texts: [whatToTranslate],
+        targetLanguageCode: 'ru',
+      }),
+    }
+  );
+  const rez = await response.json();
+  return rez;
 }
-// howto()
-const { URLSearchParams } = require('url');
-const params = new URLSearchParams();
-const text = 'Добрый день, у нас новая акция! Пицца Добряк сегодня за полцены, закажите по телефону 222. Уже ждем вашего заказа :) '
 
-params.append('text', text);
-params.append('voice', 'zahar');
-params.append('emotion', 'good');
-params.append('lang', 'ru-RU');
-params.append('speed', '1.0');
-params.append('format', 'oggopus');
-params.append('folderId', 'b1gnfe4dlmtcvgp604dj');
+async function allInOneDone() {
+  return new Promise(async (resolve) => {
+    let newIpigraf = await getTheLineOfNewEpigraf(wordsOnly);
+    console.log(newIpigraf);
+    let translatedIpigraf = await letsTranslateThePhraseIntoRussian(newIpigraf);
 
-async function sintezRechi(){
-  fetch("https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize",
-  {
-    method: 'POST',
-    headers:{
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": `Bearer ${key}`
-    },
-    body: params,})
-    // {
-  //   folderId: "b1gnfe4dlmtcvgp604dj",
-  //   text: "How are you?",
-  //   lang: "ru-RU",
-  //   format:"oggopus"
-  //   }
-  // })
-  .then(res=>{
-  const dest = fs.createWriteStream('./speech.ogg')
-  res.body.pipe(dest)
-  })
+    const { URLSearchParams } = require('url');
+    const params = new URLSearchParams();
+    params.append('text', translatedIpigraf.translations[0].text);
+    params.append('voice', 'zahar');
+    params.append('emotion', 'good');
+    params.append('lang', 'ru-RU');
+    params.append('speed', '1.0');
+    params.append('format', 'oggopus');
+    params.append('folderId', process.env.FOLDER_ID);
+    async function sintezRechi() {
+      fetch('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Bearer ${key}`,
+        },
+        body: params,
+      }).then((res) => {
+        const dest = fs.createWriteStream('./speech.ogg');
+        res.body.pipe(dest);
+        dest.on('finish', resolve);
+      });
+    }
+    await sintezRechi();
+  });
 }
-sintezRechi()
---
+++ db.js
@@ -0,0 +1,27 @@
+const db = {
+  'Побег из Шоушенка':'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+  'Темный рыцарь': "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+  "Список Шиндлера": "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+  "Криминальное чтиво":"The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+  "Форест Гамп":"The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+  "Начало":"A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+  "Матрица":"When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
+  "Славные парни":"The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
+  "Пролетая над гнездом кукушки":"A criminal pleads insanity and is admitted to a mental institution, where he rebels against the oppressive nurse and rallies up the scared patients.",
+  "Семь":"Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives.",
+  "Молчание ягнят":"A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
+  "Спасти рядового Райана":"Following the Normandy Landings, a group of soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.",
+  "Интерстеллар":"A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  "Леон":"Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. An unusual relationship forms as she becomes his protégée and learns the assassin's trade.",
+  "Король лев":"Lion prince and his father are targeted by his bitter uncle, who wants to ascend the throne himself.",
+  "Гладиатор":"A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+  "Джокер":"In City, mentally troubled comedian is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego.",
+  "Мстители: Война бесконечности":"The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful titan before his blitz of devastation and ruin puts an end to the universe.",
+  "Однажды в Америке":"A former Prohibition-era Jewish gangster returns to the Lower East Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life."
+}
+const massivFilmov = Object.keys(db)
+
+module.exports = {
+  db,
+  massivFilmov
+}
