4d583328836e6ef64966123aaffd7744d08e3fa2
Stanislav Grishin <stanislav.v.grishin@gmail.com>
2020-12-06 21:05:38 +0300

    Minor changes in DB

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const db = {
   'Побег из Шоушенка':
     'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
   'Темный рыцарь':
-    'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    'When the menace known as the Joker wreaks havoc and chaos on the people of city, superhero must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
   'Список Шиндлера':
-    'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
+    'In German-occupied Poland during World War 2, one industrialist gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
   'Криминальное чтиво':
     'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
   'Форест Гамп':
@@ -12,9 +12,9 @@ const db = {
   Начало:
     'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.',
   Матрица:
-    'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.',
+    'When a beautiful stranger leads computer hacker to a forbidding underworld, he discovers the shocking truth - the life he knows is the elaborate deception of an evil cyber-intelligence.',
   'Славные парни':
-    'The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.',
+    'The story of one criminal and his life in the mob, covering his relationship with his wife and his mob partners in the Italian-American crime syndicate.',
   'Пролетая над гнездом кукушки':
     'A criminal pleads insanity and is admitted to a mental institution, where he rebels against the oppressive nurse and rallies up the scared patients.',
   Семь:
@@ -26,17 +26,17 @@ const db = {
   Интерстеллар:
     "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
   Леон:
-    "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. An unusual relationship forms as she becomes his protégée and learns the assassin's trade.",
+    "A 12-year-old girl, is reluctantly taken in by the guy, a professional assassin, after her family is murdered. An unusual relationship forms as she becomes his protégée and learns the assassin's trade.",
   'Король лев':
     'Lion prince and his father are targeted by his bitter uncle, who wants to ascend the throne himself.',
   Гладиатор:
     'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
   Джокер:
-    'In City, mentally troubled comedian is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego.',
+    'Mentally troubled comedian is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego.',
   'Мстители: Война бесконечности':
     'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful titan before his blitz of devastation and ruin puts an end to the universe.',
   'Однажды в Америке':
-    'A former Prohibition-era Jewish gangster returns to the Lower East Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life.',
+    'A former Prohibition-era Jewish gangster returns to the Lower-East-Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life.',
 };
 const massivFilmov = Object.keys(db);
 const prepositions = [
@@ -118,7 +118,6 @@ const prepositions = [
   'am',
   'and',
   'an',
-
 ];
 
 module.exports = {
